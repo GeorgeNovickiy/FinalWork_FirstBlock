8a57e2d4d70361e94ff21de6e982b84af45b5b1a
George <madeinerussia@yandex.ru>
2023-02-15 08:52:31 +0500

    Добавили в Program.cs реализацию метода создания массива

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] CreateArrayString() // Метод создания массива
+{
+    Console.WriteLine("Введите размер массива:");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] arr = new string[size]; // по умолчанию создается массив размером size, заполненный нулями.
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива:");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
