531cd6c1527548496be070ea4e60398f67ece864
George <madeinerussia@yandex.ru>
2023-02-15 08:53:49 +0500

    Добавили в Program.cs реализацию метода вывода массива на экран

--- Project/Program.cs
+++ Project/Program.cs
@@ -11,3 +11,15 @@
     }
     return arr;
 }
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
